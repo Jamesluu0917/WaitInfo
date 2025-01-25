85e86a9a4dbd82c8c794bc3fd5347552efee7b14
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2025-01-25 17:19:32 -0500

    Add text to Home

--- wait-info/src/pages/Home.tsx
+++ wait-info/src/pages/Home.tsx
@@ -4,7 +4,10 @@ function Home() {
   return (
     <>
       <div>
-        <h1>Home</h1>
+        <h1><span style={{ color: '#6BBAEC' }}>Wait</span>Info</h1>
+        <h2>Welcome to the waiting room!</h2>
+        <p>Enter your Patient ID to start tracking</p>
+        <input type="text" placeholder="#PatientID" />
       </div>
     </>
   )
