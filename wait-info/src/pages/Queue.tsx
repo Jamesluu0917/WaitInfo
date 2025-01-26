750b82541f2ad9ef052f548a34e50dbb8c9eb737
James Luu <jamesluu0917@gmail.com>
2025-01-25 19:27:29 -0500

    created addpatientpopup

--- wait-info/src/pages/Queue.tsx
+++ wait-info/src/pages/Queue.tsx
@@ -2,11 +2,12 @@ import "./../App.css";
 
 import React from "react";
 import WaitTime from "../components/WaitTime";
+import AddPatientPopup from "../components/AddPatientPopup";
 
 const Queue = () => {
   return (
     <div className="p-4">
-      <WaitTime time="45 minutes" />
+      <AddPatientPopup />
     </div>
   );
 };
